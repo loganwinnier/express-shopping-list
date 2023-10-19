cf2909e86e235ca4d45896f94ee388889a498017
Jessica Biondi <jbiondi.cs@gmail.com>
2023-10-18 20:15:14 -0400

    added more routes like patch and delete

--- fakeDb.js
+++ fakeDb.js
@@ -1,5 +1,5 @@
 
 
-const items = [];
+const items = [{name: 'popsicles', price: 5.00}, {name: 'ice-cream', price: 4.00}];
 
 module.exports = { items };
--- itemsRoutes.js
+++ itemsRoutes.js
@@ -1,30 +1,86 @@
 const express = require("express");
 
 const { items } = require("./fakeDb");
+const { BadRequestError } = require("./expressError");
 const router = new express.Router();
 
+
+/**
+ *
+ */
 router.get("", function (req, res) {
 
     return res.json({ items: items });
 
-})
+});
 
+
+/**
+ *
+ */
 router.post("", function (req, res) {
     const item = req.body;
-    console.log(items)
+
+    item.name = item.name.replace(' ', '-');
+
     items.push(item);
     res.json({ added: item });
-})
+});
 
+
+/**
+ *
+ */
 router.get("/:name", function (req, res) {
     const name = req.params.name;
-    const item = items.find(item => item === item[name])
-    if (item) {
+    const item = items.find(item => item === item[name]);
 
+    if (!item) {
+        throw new BadRequestError('Item not found :(');
     }
-    res.json({ items: items })
 
-})
+    return res.json({ items: items });
+
+});
+
+
+/**
+ *
+ */
+router.patch("/:name", function (req, res) {
+    const name = req.params.name;
+
+    const itemIndex = items.findIndex(itm => name === itm['name']);
+
+    if (itemIndex === -1) {
+        throw new BadRequestError('Item not found :(');
+    }
+
+    const item = req.body;
+
+    items[itemIndex] = item;
+
+    return res.json({ updated: items[itemIndex] });
+
+});
+
+
+/**
+ *
+ */
+router.delete('/:name', function (req, res) {
+    const name = req.params.name;
+
+    const itemIndex = items.findIndex(itm => name === itm['name']);
+
+    if (itemIndex === -1) {
+        throw new BadRequestError('Item not found :(');
+    }
+
+    items.splice(itemIndex, 1);
+
+    return res.json({ message: "Deleted" });
+});
 
 
 //Bottom of file
